7126c649a08e9aca02bebe6bccfe8fd0910b67c8
Nicholas Kipkoech <nickey968@gmail.com>
2024-09-25 09:34:02 +0300

    refactor the jwt function

--- src/utils/jwt-utils.ts
+++ src/utils/jwt-utils.ts
@@ -4,13 +4,18 @@ import { IUser } from "../interfaces/users";
 
 config();
 
-const jwt_secret = process.env.JWT_SECRET as string;
-
-const createToken = (user: IUser) => {
-  const token = jwt.sign(user, jwt_secret, {
-    expiresIn: "5h",
-  });
-  return token;
-};
-
-export default createToken;
+export class JWTService {
+  private jwt_secret: string;
+  constructor() {
+    if (!process.env.JWT_SECRET) {
+      throw new Error("JWT secret is not defined in enviroment variables!");
+    }
+    this.jwt_secret = process.env.JWT_SECRET as string;
+  }
+  createToken = (user: IUser) => {
+    const token = jwt.sign(user, this.jwt_secret, {
+      expiresIn: "5h",
+    });
+    return token;
+  };
+}
