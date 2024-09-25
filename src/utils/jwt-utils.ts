89c9c6ecaf496c2a3062239cbfbc296111f82db3
Nicholas Kipkoech <nickey968@gmail.com>
2024-09-25 09:46:11 +0300

    omit password from user payload

--- src/utils/jwt-utils.ts
+++ src/utils/jwt-utils.ts
@@ -4,18 +4,21 @@ import { IUser } from "../interfaces/users";
 
 config();
 
+type User = Omit<IUser, "password">;
+
 export class JWTService {
   private jwt_secret: string;
+
   constructor() {
     if (!process.env.JWT_SECRET) {
       throw new Error("JWT secret is not defined in enviroment variables!");
     }
     this.jwt_secret = process.env.JWT_SECRET as string;
   }
-  createToken = (user: IUser) => {
+  createToken(user: User): string {
     const token = jwt.sign(user, this.jwt_secret, {
       expiresIn: "5h",
     });
     return token;
-  };
+  }
 }
