7d60ac11ad1fcff9ea863ab53c14985c49363fe6
Nicholas Kipkoech <nickey968@gmail.com>
2024-10-07 10:32:07 +0300

    add authentication module

--- src/config/database-config.ts
+++ src/config/database-config.ts
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
-
+import oracledb from "oracledb";
 export class MongodbConnection {
   static async connect(dbUrl: string) {
     try {
@@ -10,3 +10,33 @@ export class MongodbConnection {
     }
   }
 }
+
+function getDbkeys() {
+  let user;
+  let password;
+  let connString;
+
+  if (process.env.ENVIROMENT === "MAYFAIR_TEST") {
+    user = process.env.MAYFAIR_DATABASE_USER;
+    password = process.env.MAYFAIR_DATABASE_PASSWORD;
+    connString = process.env.MAYFAIR_DATABASE_CONN_STRING;
+  } else if (process.env.ENVIROMENT === "INTRA") {
+    user = process.env.INTRA_DATABASE_USER;
+    password = process.env.INTRA_DATABASE_PASSWORD;
+    connString = process.env.INTRA_DATABASE_CONN_STRING;
+  }
+  return { user, password, connString };
+}
+
+const { user, password, connString } = getDbkeys();
+// Create and export the connection pool
+export const pool = oracledb.createPool({
+  user: user,
+  password: password,
+  connectionString: connString,
+  poolMin: 5,
+  poolMax: 5,
+  poolIncrement: 5,
+});
+
+export default pool;
--- src/interfaces/users.ts
+++ src/interfaces/users.ts
@@ -1,13 +1,6 @@
 type Roles = "Superadmin" | "claims manager" | "claims office" | "normal user";
 
 export interface IUser {
-  name: string;
-  email: string;
+  username: string;
   password: string;
-  verified: boolean;
-  active: boolean;
-  roles: Roles[];
-  avatar?: string;
-  createdAt?: Date;
-  deletedAt?: Date;
 }
